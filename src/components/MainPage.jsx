a494742331347d677f6d5ca70341f9a1ad02bf0e
Irina <irinamalkush@gmail.com>
2020-06-19 14:57:25 +0300

    MainTable with styles

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
 import { Url } from "./Url";
 import { useSelector, useDispatch } from "react-redux";
+import "./stylesMainPage.css";
 
 export default function MainPage() {
-  
   useEffect(() => {
-    MakeRequestToServer(1)
-  }, [])
+    MakeRequestToServer(1);
+  }, []);
 
   const dispatch = useDispatch();
 
   async function MakeRequestToServer(pageNumber) {
-    
     let response = await fetch(Url + `?page=${pageNumber}`);
     const data = await response.json();
 
@@ -19,7 +18,7 @@ export default function MainPage() {
       type: "CHANGE_ITEMS",
       payload: data.items,
     };
-  
+
     const actionChangeTotal = {
       type: "CHANGE_TOTAL",
       payload: data.total,
@@ -29,37 +28,50 @@ export default function MainPage() {
     dispatch(actionChangeItems);
   }
 
-  const CreateTable = () => {  
-    const items = useSelector(state => state.items);
+  const CreateTable = () => {
+    const items = useSelector((state) => state.items);
 
     if (items == null)
       return (
-        <div>
-            <div>Загрузка...</div>
-        </div>
-      )
+        <tbody>
+          <tr><td>Загрузка...</td></tr>
+        </tbody>
+      );
     else
       return (
-        <div>
-            {items.map((item) => {
+        <tbody>
+          {items.map((item) => {
             return (
-              <div>
-                <div>{item.created}</div>
-                <div>{item.name}</div>
-                <div>{item.status}</div>
-                <div>{item.succeed}</div>
-                <div>{item.lastError}</div>
-              </div>)})}
-        </div>
+              <tr key={item.id}>
+                <td>{item.created}</td>
+                <td>{item.name}</td>
+                <td>{item.status}</td>
+                <td>{item.succeed}</td>
+                <td>{item.lastError}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       );
   };
 
   return (
-    <div>
+    <div className="main-table">
       <div>
         <p>Статус загрузки файлов</p>
       </div>
-      <div>{CreateTable()}</div>
+      <table>
+        <thead>
+          <tr>
+            <th>добавлен</th>
+            <th>имя файла</th>
+            <th>состояние</th>
+            <th>завершено</th>
+            <th>последняя ошибка</th>
+          </tr>
+        </thead>
+        {CreateTable()}
+      </table>
     </div>
   );
 }
